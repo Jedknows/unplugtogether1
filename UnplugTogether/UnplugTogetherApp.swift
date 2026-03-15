b8b9e534797bd9001a14f095f5e97076cdffd84f
Jed Siegel <Jedsiegel5@gmail.com>
2026-03-14 23:24:48 -0700

    Initial Commit

---
+++ UnplugTogether/UnplugTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnplugTogetherApp.swift
+//  UnplugTogether
+//
+//  Created by Jed Siegel on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct UnplugTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
